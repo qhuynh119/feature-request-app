d19c028e8d636f28cae6c06e141a19c6e96502af
Quan Huynh <qhuynh119@gmail.com>
2016-02-23 23:41:37 -0600

    Added navigation bar with "Sign In" button

--- index.php
+++ index.php
@@ -1,8 +1,36 @@
+<?php
+/**************************************************
+ *** FEATURE REQUEST APP
+ *** author: Quan Huynh
+ **************************************************/
+?>
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
 	<title>Feature Request App</title>
+
+	<!-- Bootstrap Core CSS -->
+	<link href="css/bootstrap.min.css" rel="stylesheet" />
+	<!-- Bootstrap Theme CSS -->
+	<link href="css/bootstrap.min.css" rel="stylesheet" />
 </head>
 <body>
+	<!-------- NAVBAR -------->
+	<nav class="navbar navbar-default navbar-fixed-top">
+		<div class="container">
+			<div class="navbar-header">
+				<a class="navbar-brand" href="#">Feature Request App</a>
+			</div><!-- .navbar-header -->
+
+			<div id="navbar" class="navbar-collapse collapse">
+				<ul class="nav navbar-nav navbar-right">
+					<li class="active"><a href="./">Sign In</a></li>
+				</ul>
+			</div><!-- #navbar .navbar-collapse -->
+		</div><!-- .container -->
+	</nav>
+
+	<!-- Bootstrap JS -->
+	<script src="js/bootstrap.min.js" type="text/javascript"></script>
 </body>
 </html>
